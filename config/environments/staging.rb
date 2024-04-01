5c893cdd7022e52094dffd654edf7641317fed02
Viktor Butsaev <linkinpark02111@mail.ru>
2024-04-01 12:51:38 +0300

    Added staging environment (#125)
    * added new env and db config
    
    * fix lint

---
+++ config/environments/staging.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require File.expand_path("production.rb", __dir__)
+
+Rails.application.configure do
+  # Here override any defaults
+  # config.serve_static_files = true
+end
