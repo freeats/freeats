3ed732ff86e5b9d14fea5edebbe7e8fc833acda4
Vladimir Bikmeev <elmakan@gmail.com>
2024-09-19 12:56:26 +0600

    Passed dummy secrets if we compile assets (#454)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,15 +94,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: "ats.toughbyte.com" }
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.sendgrid.username!,
-  #   password: Rails.application.credentials.sendgrid.password!,
-  #   address: "smtp.sendgrid.net",
-  #   port: 587,
-  #   domain: "ats.toughbyte.com",
-  #   authentication: "plain",
-  #   enable_starttls_auto: true
-  # }
+  smtp_user_name, smtp_password =
+    if ENV["SECRET_KEY_BASE_DUMMY"].blank?
+      [Rails.application.credentials.sendgrid.username!,
+       Rails.application.credentials.sendgrid.password!]
+    else
+      %w[dummy dummy]
+    end
+  config.action_mailer.smtp_settings = {
+    user_name: smtp_user_name,
+    password: smtp_password,
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "ats.toughbyte.com",
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
