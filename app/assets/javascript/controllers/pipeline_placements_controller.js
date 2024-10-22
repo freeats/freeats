f27805d53fd98f57494a1a2c9528fa96d1080371
Konstantin Kanashchuk <41356747+ksnitsky@users.noreply.github.com>
2024-10-22 10:53:23 +0600

    disqualify button always visible on pipeline when disqualify dropdown is shown (#689)
    * span as ruby object instead of slim tag
    
    and new data actions
    
    * draft stiumus actions instead of old useless code
    
    * btn styles under .card-right-container
    
    * new target and action name

--- app/assets/javascript/controllers/pipeline_placements_controller.js
+++ app/assets/javascript/controllers/pipeline_placements_controller.js
@@ -1,17 +1,9 @@
 import { Controller } from "@hotwired/stimulus";
-import $ from "jquery";
-import { initBootstrapSelect } from "../src/shared/bootstrap_select";
-import { removeTooltips } from "../src/shared/tooltips";
 
 export default class extends Controller {
-  connect() {
-    $(this.element).find("form").submit(
-      (event) => {
-        const $form = $(event.target);
-        $form.find(":input[type=submit]").prop("disabled", true);
-      },
-    );
-    initBootstrapSelect($(this.element).find(".selectpicker"));
-    removeTooltips();
+  static targets = ["disqualifyButtonHiddenContainer"];
+
+  onDisqualifyDropdownToggle() {
+    this.disqualifyButtonHiddenContainerTarget.classList.toggle("card-hidden-container");
   }
 }
