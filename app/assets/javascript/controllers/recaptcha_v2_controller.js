42f2a942f1af7d8e118ca212ab040b89a6873fe6
Liamshin Ilia <lyamsh@yandex.kz>
2024-10-24 11:00:24 +0300

    Fixed Recaptcha (#715)

--- app/assets/javascript/controllers/recaptcha_v2_controller.js
+++ app/assets/javascript/controllers/recaptcha_v2_controller.js
@@ -3,13 +3,16 @@ import { Modal } from "bootstrap";
 import { Turbo } from "@hotwired/turbo-rails";
 
 export default class extends Controller {
+
+  static targets = ["form"];
+
   connect() {
     const modal = new Modal(this.element, {});
 
     modal.show();
 
     window.recaptchaSubmit = function recaptchaSubmit() {
-      const form = document.getElementById("register-form");
+      const form = this.formTarget;
 
       form.setAttribute("verified", "true");
       Turbo.navigator.submitForm(form);
