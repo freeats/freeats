2bcb8f8c26fe8f33f09e134aea1a22a797d56738
Liamshin Ilia <lyamsh@yandex.kz>
2024-10-24 12:19:08 +0300

    Revert "Fixed Recaptcha" (#724)
    This reverts commit 42f2a942f1af7d8e118ca212ab040b89a6873fe6.

--- app/assets/javascript/controllers/recaptcha_v2_controller.js
+++ app/assets/javascript/controllers/recaptcha_v2_controller.js
@@ -3,16 +3,13 @@ import { Modal } from "bootstrap";
 import { Turbo } from "@hotwired/turbo-rails";
 
 export default class extends Controller {
-
-  static targets = ["form"];
-
   connect() {
     const modal = new Modal(this.element, {});
 
     modal.show();
 
     window.recaptchaSubmit = function recaptchaSubmit() {
-      const form = this.formTarget;
+      const form = document.getElementById("register-form");
 
       form.setAttribute("verified", "true");
       Turbo.navigator.submitForm(form);
