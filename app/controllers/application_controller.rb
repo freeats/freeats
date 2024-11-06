dd9f72bfbdc349430b1873ee2b493c798fdfe58b
Ilia Liamshin <lyamsh@yandex.kz>
2024-11-06 18:48:08 +0300

    Used controller constant instead of ENV in view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   include ErrorHandler
 
+  CLARIFY_KEY = ENV.fetch("CLARIFY_KEY", nil)
+
   before_action :set_sentry_account_context
   before_action :set_sentry_context
   before_action :set_selector_id_for_page
