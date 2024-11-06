26bde0fe6cd0575400f3e427ae6ab51400fde282
PrinceYauj <c2h5ohmel@yandex.ru>
2024-11-06 18:07:51 +0300

    Add rake task to update placement_changed events

---
+++ lib/tasks/events.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+namespace :events do
+  task update_placement_changed_events: :environment do
+    Log.info("Updating placement_changed events...")
+
+    Event
+      .where(type: "placement_changed")
+      .where("changed_to NOT IN ('qualified', 'reserved') OR " \
+             "changed_from NOT IN ('qualified', 'reserved')")
+      .where("changed_to != 'disqualified' AND changed_from != 'disqualified'")
+      .find_each do |event|
+        update_params = {}
+        unless event.changed_to.in?(%w[qualified reserved])
+          update_params[:changed_to] = "disqualified"
+          update_params[:properties] = { title: event.changed_to }
+        end
+        unless event.changed_from.in?(%w[qualified reserved])
+          update_params[:changed_from] = "disqualified"
+        end
+
+        event.update!(update_params)
+      end
+
+    Log.info("Done.")
+  end
+end
