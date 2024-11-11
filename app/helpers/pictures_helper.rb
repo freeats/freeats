a28859a9cc6b19e910c41408a34b17789f186c74
wrmk <elmakan@gmail.com>
2024-11-11 11:20:59 +0600

    Stash changes

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -3,14 +3,8 @@
 # Use <picture> HTML element to show different versions of an image,
 # or when an image is combined with text in a view.
 module PicturesHelper
-  def picture_avatar_icon(attachment, helper_opts = {}, html_opts = {})
-    opts = html_opts
-    hopts = { lazy: false }.merge(helper_opts)
+  def picture_avatar_icon(attachment, opts = {})
     src_sym = :src
-    if hopts[:lazy]
-      opts = html_opts.merge(class: "lazy") { |_, old, new| "#{old} #{new}" }
-      src_sym = :"data-src"
-    end
 
     opts[:class] = ["avatar", "avatar-sm", *opts.delete(:class)]
 
