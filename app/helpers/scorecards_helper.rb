9620f961ff0416315a81f5e2717ee971a93b13b8
ksnitsky <ksnitsky@gmail.com>
2024-11-11 16:25:51 +0600

    refactor `ScorecardsHelper#score_icon`

--- app/helpers/scorecards_helper.rb
+++ app/helpers/scorecards_helper.rb
@@ -2,50 +2,19 @@
 
 module ScorecardsHelper
   def score_icon(score, with_text: false)
-    case score
-    when "irrelevant"
-      label_tag(:score_irrelevant, class: "text-danger") do
-        concat(render(IconComponent.new(:mood_sad, icon_type: :filled)))
-        if with_text
-          concat(
-            content_tag(:span, class: "ms-2") do
-              t("candidates.advancement.irrelevant")
-            end
-          )
-        end
-      end
-    when "relevant"
-      label_tag(:score_relevant, class: "text-warning") do
-        concat(render(IconComponent.new(:mood_empty, icon_type: :filled)))
-        if with_text
-          concat(
-            content_tag(:span, class: "ms-2") do
-              t("candidates.advancement.might_be_irrelevant")
-            end
-          )
-        end
-      end
-    when "good"
-      label_tag(:score_good, class: "text-success") do
-        concat(render(IconComponent.new(:mood_smile, icon_type: :filled)))
-        if with_text
-          concat(
-            content_tag(:span, class: "ms-2") do
-              t("candidates.advancement.good_candidate")
-            end
-          )
-        end
-      end
-    when "perfect"
-      label_tag(:score_perfect, class: "text-info") do
-        concat(render(IconComponent.new(:mood_happy, icon_type: :filled)))
-        if with_text
-          concat(
-            content_tag(:span, class: "ms-2") do
-              t("candidates.advancement.perfect_candidate")
-            end
-          )
-        end
+    icon, color = {
+      irrelevant: [:mood_sad, "text-red"],
+      relevant: [:mood_empty, "text-red-300"],
+      good: [:mood_smile, "text-green-300"],
+      perfect: [:mood_happy, "text-green"]
+    }[score&.to_sym]
+
+    content_tag(:span, class: ["d-flex align-items-center gap-2", color]) do
+      concat(render(IconComponent.new(icon, icon_type: :filled)))
+      if with_text
+        concat(
+          t("candidates.advancement.#{score}_candidate")
+        )
       end
     end
   end
