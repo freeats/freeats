a5c0cd6e70d1c1ad4e09222d0acb2bb370214a82
PrinceYauj <c2h5ohmel@yandex.ru>
2024-11-12 15:58:42 +0300

    Update operation

--- app/operations/placements/change_status.rb
+++ app/operations/placements/change_status.rb
@@ -3,7 +3,8 @@
 class Placements::ChangeStatus < ApplicationOperation
   include Dry::Monads[:result, :do]
 
-  option :new_status, Types::Strict::String
+  option :new_status, Types::Strict::String.enum(*Placement.statuses.keys)
+  option :disqualify_reason_id, Types::Coercible::Integer.optional, optional: true
   option :placement, Types::Instance(Placement)
   option :actor_account, Types::Instance(Account).optional, optional: true
 
@@ -11,6 +12,12 @@ class Placements::ChangeStatus < ApplicationOperation
     old_status = placement.status
 
     return Success(placement) if old_status == new_status
+
+    disqualify_reason = DisqualifyReason.find_by(id: disqualify_reason_id)
+    if new_status == "disqualified" && disqualify_reason.blank?
+      return Failure[:disqualify_reason_invalid,
+                     I18n.t("candidates.disqualification.reason_not_found")]
+    end
 
     placement_changed_params = {
       actor_account:,
@@ -21,19 +28,16 @@ class Placements::ChangeStatus < ApplicationOperation
       changed_to: new_status
     }
 
-    if (add_disqualify_reason = %w[qualified reserved].exclude?(new_status))
-      placement.status = "disqualified"
-      placement_changed_params[:properties] = { reason: new_status.humanize }
+    placement.status = new_status
+
+    if new_status == "disqualified"
+      placement_changed_params[:properties] = { reason: disqualify_reason.title }
+      placement.disqualify_reason_id = disqualify_reason.id
     else
-      placement.status = new_status
       placement.disqualify_reason_id = nil
     end
 
     ActiveRecord::Base.transaction do
-      if add_disqualify_reason
-        disqualify_reason = yield find_disqualify_reason(new_status)
-        placement.disqualify_reason_id = disqualify_reason.id
-      end
       yield save_placement(placement)
       yield Events::Add.new(params: placement_changed_params).call
     end
@@ -49,13 +53,5 @@ class Placements::ChangeStatus < ApplicationOperation
     Success()
   rescue ActiveRecord::RecordInvalid => e
     Failure[:placement_invalid, placement.errors.full_messages.presence || e.to_s]
-  end
-
-  def find_disqualify_reason(reason)
-    disqualify_reason = DisqualifyReason.find_by(title: reason.humanize)
-
-    return Success(disqualify_reason) if disqualify_reason.present?
-
-    Failure[:disqualify_reason_invalid, I18n.t("placements.reason_not_found")]
   end
 end
