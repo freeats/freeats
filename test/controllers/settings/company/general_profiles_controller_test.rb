077eb3d6e5db99b6733d7e9911de6dda1eae8a9e
Ilia Liamshin <lyamsh@yandex.kz>
2024-11-27 18:49:26 +0300

    feat: Created general company page on Admin interface

--- test/controllers/settings/company/general_profiles_controller_test.rb
+++ test/controllers/settings/company/general_profiles_controller_test.rb
@@ -4,11 +4,31 @@ require "test_helper"
 
 class Settings::Company::GeneralProfilesControllerTest < ActionDispatch::IntegrationTest
   test "should open general company settings" do
-    skip "TODO: Functionality in the process of implementation."
-    sign_in accounts(:interviewer_account)
+    sign_in accounts(:admin_account)
 
     get settings_company_general_path
 
     assert_response :success
+  end
+
+  test "should update company name if name is valid" do
+    sign_in accounts(:admin_account)
+
+    new_valid_name = "New Name"
+
+    patch settings_company_general_path(tenant: { name: new_valid_name })
+
+    assert_response :success
+
+    new_invalid_name = " "
+
+    err = assert_raises(RenderErrorExceptionForTests) do
+      patch(settings_company_general_path(tenant: { name: new_invalid_name }))
+    end
+
+    err_info = JSON.parse(err.message)
+
+    assert_equal err_info["message"], ["Name can't be blank"]
+    assert_equal err_info["status"], "bad_request"
   end
 end
