9e673a213b6d4440079bc4565490b0bba4939574
wrmk <elmakan@gmail.com>
2024-12-04 14:44:04 +0600

    Clarified policy

--- app/policies/settings/recruitment/email_templates_policy.rb
+++ app/policies/settings/recruitment/email_templates_policy.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Settings::Recruitment::EmailTemplatesPolicy < ApplicationPolicy
-  alias_rule :index?, :show?, :new?, to: :available_for_admin_on_development?
+  alias_rule :index?, :show?, :new?, to: :available_for_admin_on_local?
 
-  def available_for_admin_on_development?
-    available_for_admin? && Rails.env.development?
+  def available_for_admin_on_local?
+    available_for_admin? && Rails.env.local?
   end
 end
