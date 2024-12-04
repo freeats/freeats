6aadc71add1683f71a5d8e46098fec3c83fd4264
wrmk <elmakan@gmail.com>
2024-12-04 14:18:27 +0600

    Updated policy to display on dev

--- app/policies/settings/recruitment/email_templates_policy.rb
+++ app/policies/settings/recruitment/email_templates_policy.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Settings::Recruitment::EmailTemplatesPolicy < ApplicationPolicy
-  alias_rule :index?, :show?, :new?, to: :available_for_admin?
+  alias_rule :index?, :show?, :new?, to: :available_for_admin_on_development?
+
+  def available_for_admin_on_development?
+    available_for_admin? && Rails.env.development?
+  end
 end
